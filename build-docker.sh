050184e354b197f3be123da7620c0e7160a46194
Marco Oesch <marco.oesch@decentage.com>
2021-05-06 20:01:31 +0200

    added release version

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,10 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-
+set
 RELEASE_VERSION=$(cat release-version.txt)
-docker build -t decentage/iep-peerexplorer-ui:latest .
+docker build -t decentage/iep-peerexplorer-ui:${RELEASE_VERSION} .
+#docker container rm -f iep-peerexplorer-ui-extract
+#docker container create --name iep-peerexplorer-ui-extract decentage/iep-peerexplorer-ui:${RELEASE_VERSION}  
+#docker container cp iep-peerexplorer-ui-extract:/build .  
+#docker container rm -f iep-peerexplorer-ui-extract
