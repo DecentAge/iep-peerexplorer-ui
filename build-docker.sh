fab81baad5d25f45646c13c2f3319fb0bcb48365
Marco Oesch <marco.oesch@decentage.com>
2021-05-27 14:20:00 +0200

    remove container after build

--- build-docker.sh
+++ build-docker.sh
@@ -6,9 +6,10 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-peerexplorer-ui:${RELEASE_VERSION} .
 
-CID=$(docker create decentage/iep-peerexplorer-ui:${RELEASE_VERSION})
+CONTAINER_ID=$(docker create --rm --name iep-peerexplorer-ui-extract decentage/iep-peerexplorer-ui:${RELEASE_VERSION})
 mkdir -p ./build
-docker cp ${CID}:/build/iep-peerexplorer-ui.zip ./build
+docker cp ${CONTAINER_ID}:/build/iep-peerexplorer-ui.zip ./build
+docker rm ${CONTAINER_ID}
 
 #docker container rm -f iep-peerexplorer-ui-extract
 #docker container create --name iep-peerexplorer-ui-extract decentage/iep-peerexplorer-ui:${RELEASE_VERSION}  
