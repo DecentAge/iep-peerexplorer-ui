fb4975009038e6f3dcba35341f8fbd3fe04b1225
daviddecentage <david.farina@decentage.com>
2021-12-07 15:06:50 +0100

    cache service requests

--- app/peers/peer-service.js
+++ app/peers/peer-service.js
@@ -16,19 +16,55 @@
 
 angular.module('peers')
     .service('PeerService', ['Restangular', 'peerConfig', '$q', function (Restangular, peerConfig, $q) {
+        var lastFetched = {
+            getPeers: {
+                params: null,
+                time: null,
+                deferred: null,
+            },
+            getStats: {
+                time: null,
+                deferred: null,
+            },
+        };
 
         this.getPeers = function (page, results) {
-            var params = {
-                'page': page,
-                'results': results,
-                'filter': 'numberOfActivePeers',
-                'order': 'desc'
-            };
-            return Restangular.all(peerConfig.peerEndPoint).customGET('', params);
-        };
+                var params = {
+                    'page': page,
+                    'results': results,
+                    'filter': 'numberOfActivePeers',
+                    'order': 'desc'
+                };
 
-        this.getStats=function(){
-            return Restangular.all('api').one('getStats').customGET('', '');
-        };
+                var lastFetch = lastFetched.getPeers.time;
+                if (lastFetched.getPeers.deferred && JSON.stringify(params) === lastFetched.getPeers.params && lastFetch !== null && new Date().getTime() - lastFetch < 1000) {
+                    return lastFetched.getPeers.deferred.promise;
+                }
+
+                lastFetched.getPeers.deferred = $q.defer();
+
+                lastFetched.getPeers.time = new Date().getTime();
+                lastFetched.getPeers.params = JSON.stringify(params);
+                Restangular.all(peerConfig.peerEndPoint).customGET('', params).then(function (resp) {
+                    lastFetched.getStats.deferred.resolve(resp);
+                });
+                return lastFetched.getPeers.deferred.promise;
+            };
+
+            this.getStats = function () {
+                var lastFetch = lastFetched.getStats.time;
+
+                if (lastFetched.getStats.deferred && lastFetch !== null && new Date().getTime() - lastFetch < 1000) {
+                    return lastFetched.getStats.deferred.promise;
+                }
+
+                lastFetched.getStats.deferred = $q.defer();
+
+                lastFetched.getStats.time = new Date().getTime();
+                Restangular.all('api').one('getStats').customGET('', '').then(function (resp) {
+                    lastFetched.getStats.deferred.resolve(resp);
+                });
+                return lastFetched.getStats.deferred.promise;
+            };
 
     }]);
